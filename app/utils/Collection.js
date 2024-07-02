ca0bfb677886405173aa8a9f072261f25d1c076a
Bagas Dwi Putra Majid <bagasdwiputramajid2003@gmail.com>
2024-07-02 08:42:59 +0700

    First Upload

---
+++ app/utils/Collection.js
@@ -0,0 +1,27 @@
+const Collection = {
+  //Logo & Icon
+  Logo: require("../../assets/images/Logo-Master-SIM-73.png"),
+  //   icon: require("../assets/images/Icon-BPK.png"),
+  Error: require("../../assets/images/404.png"),
+  Profile: require("../../assets/images/Profile.png"),
+
+  //Events
+  Event1: require("../../assets/images/events/event1.png"),
+  Event2: require("../../assets/images/events/event2.png"),
+  Event3: require("../../assets/images/events/event3.png"),
+
+  //Services
+  Payroll: require("../../assets/images/services/payroll.png"),
+  History: require("../../assets/images/services/history.png"),
+  Poliklinik: require("../../assets/images/services/poliklinik.png"),
+  Ticketing: require("../../assets/images/services/ticketing.png"),
+
+  //Lottie
+  Lottie_Collaboration: require("../../assets/lottie/collaboration.json"),
+  Lottie_Puzzle: require("../../assets/lottie/puzzle.json"),
+  Lottie_Work: require("../../assets/lottie/work.json"),
+  Lottie_Close: require("../../assets/lottie/close.json"),
+  Lottie_Checklist: require("../../assets/lottie/checklist.json"),
+};
+
+export default Collection;
---
+++ app/utils/Color.js
@@ -0,0 +1,23 @@
+export default {
+  Primary: "#5572B7",
+
+  Black: "#000",
+
+  White: "#FFF",
+
+  Grey: "#EDEDED",
+  
+  GreyText: "#9a9a9a",
+
+  Yellow: "#FFDF28",
+
+  subYellow: "#FFCB31",
+
+  Blue: "#2A65FF",
+
+  Red: "#FF5454",
+
+  Green: "#19A337",
+
+  Pink: "#FF008A",
+};
